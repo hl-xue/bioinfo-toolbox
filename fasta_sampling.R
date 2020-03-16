ad36069121a8896166f7407d1cdc3695852b5a0d
haoliang.xue <haoliang.xue@i2bc.paris-saclay.fr>
2020-03-16 18:55:56 +0100

    fasta_sampling.R executable

---
+++ fasta_sampling.R
@@ -0,0 +1,22 @@
+rm(list = ls())
+
+library(Biostrings)
+
+args <- commandArgs(trailingOnly = T)
+fa_to_sample <- args[1]
+output_path <- args[2]
+min_seq_length <- args[3]
+n_sample <- args[4]
+
+
+print(paste("Fasta to sample:", fa_to_sample))
+print(paste("Output path:", output_path))
+print(paste("Minimum sequence length:", min_seq_length))
+print(paste("Sampling number:", n_sample))
+
+biostr <- readDNAStringSet(fa_to_sample)
+biostr <- biostr[width(biostr) > min_seq_length]
+ind <- sample(1:length(biostr), size = n_sample, replace = F)
+biostr.sel <- biostr[ind]
+
+writeXStringSet(biostr.sel, output_path)
