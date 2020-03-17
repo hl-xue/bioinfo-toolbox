aadec76e279c179a7bdc9b367b441bcb16f41bc7
haoliang.xue <haoliang.xue@i2bc.paris-saclay.fr>
2020-03-17 10:13:33 +0100

    output oneline fasta

--- fasta_sampling.R
+++ fasta_sampling.R
@@ -8,7 +8,6 @@ output_path <- args[2]
 min_seq_length <- args[3]
 n_sample <- args[4]
 
-
 print(paste("Fasta to sample:", fa_to_sample))
 print(paste("Output path:", output_path))
 print(paste("Minimum sequence length:", min_seq_length))
@@ -19,4 +18,4 @@ biostr <- biostr[width(biostr) > min_seq_length]
 ind <- sample(1:length(biostr), size = n_sample, replace = F)
 biostr.sel <- biostr[ind]
 
-writeXStringSet(biostr.sel, output_path)
+writeXStringSet(biostr.sel, output_path, width = 20001)
